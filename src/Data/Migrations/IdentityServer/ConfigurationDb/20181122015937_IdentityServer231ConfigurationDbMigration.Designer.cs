973be588fcfc62d76667d35051d5295b6ae44a93
Andy Miller <andy@andyfmiller.com>
2018-11-21 18:02:08 -0800

    Update IdentityServer4

--- src/Data/Migrations/IdentityServer/ConfigurationDb/20181122015937_IdentityServer231ConfigurationDbMigration.Designer.cs
+++ src/Data/Migrations/IdentityServer/ConfigurationDb/20181122015937_IdentityServer231ConfigurationDbMigration.Designer.cs
@@ -4,14 +4,16 @@ using IdentityServer4.EntityFramework.DbContexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace AdvantagePlatform.Data.Migrations.IdentityServer.ConfigurationDb
 {
     [DbContext(typeof(ConfigurationDbContext))]
-    partial class ConfigurationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20181122015937_IdentityServer231ConfigurationDbMigration")]
+    partial class IdentityServer231ConfigurationDbMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
