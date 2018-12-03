5b25292de510be611cb9034582294e57753d7282
Andy Miller <andy@andyfmiller.com>
2018-12-02 17:26:29 -0800

    Add Ags results controller.

---
+++ src/Controllers/LineItemController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using AdvantagePlatform.Data;
+using LtiAdvantage.AssignmentGradeServices;
+using LtiAdvantage.IdentityServer4;
+using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace AdvantagePlatform.Controllers
+{
+    public class LineItemController : LineItemControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public LineItemController(
+            ILogger<LineItemsControllerBase> logger, 
+            ApplicationDbContext context) : base(logger)
+        {
+            _context = context;
+        }
+
+        protected override Task<LineItemResult> OnCreateLineItemAsync(PostLineItemRequest request)
+        {
+            var lineItem = request.LineItem;
+            lineItem.Id = lineItem.GetHashCode().ToString();
+            return Task.FromResult(Created(lineItem));
+        }
+
+        protected override Task<IActionResult> OnDeleteLineItemAsync(DeleteLineItemRequest request)
+        {
+            return Task.FromResult(new OkResult() as IActionResult);
+        }
+
+        protected override async Task<LineItemResult> OnGetLineItemAsync(GetLineItemRequest request)
+        {
+            var course = await _context.GetCourseByContextIdAsync(request.ContextId);
+            if (course == null)
+            {
+                return NotFound(default(LineItem));
+            }
+
+            var gradebookColumn = course.GradebookColumns.SingleOrDefault(c => c.Id == Convert.ToInt32(request.Id));
+            if (gradebookColumn == null)
+            {
+                return NotFound(default(LineItem));
+            }
+
+            return Ok(new LineItem
+            {
+                Id = Request.GetDisplayUrl().EnsureTrailingSlash() + gradebookColumn.Id,
+                EndDateTime = gradebookColumn.EndDateTime,
+                Label = gradebookColumn.Label,
+                ResourceId = gradebookColumn.ResourceId,
+                ResourceLinkId = gradebookColumn.ResourceLink.Id.ToString(),
+                ScoreMaximum = gradebookColumn.ScoreMaximum,
+                StartDateTime = gradebookColumn.StartDateTime,
+                Tag = gradebookColumn.Tag
+            });
+        }
+
+        protected override Task<IActionResult> OnUpdateLineItemAsync(PutLineItemRequest request)
+        {
+            return Task.FromResult(new OkResult() as IActionResult);
+        }
+    }
+}
---
+++ src/Controllers/ResultsController.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+using LtiAdvantage.AssignmentGradeServices;
+using Microsoft.Extensions.Logging;
+
+namespace AdvantagePlatform.Controllers
+{
+    public class ResultsController : ResultsControllerBase
+    {
+        private readonly ILogger<ResultsControllerBase> _logger;
+
+        public ResultsController(ILogger<ResultsControllerBase> logger) : base(logger)
+        {
+            _logger = logger;
+        }
+
+        protected override Task<ResultContainerResult> OnGetResultsAsync(GetResultsRequest request)
+        {
+            try
+            {
+                _logger.LogInformation($"Starting {nameof(OnGetResultsAsync)}.");
+
+                // Since there are no scores, there are no results.
+                return Task.FromResult(new ResultContainerResult(new ResultContainer()));
+            }
+            finally 
+            {
+                _logger.LogInformation($"Exiting {nameof(OnGetResultsAsync)}.");
+            }
+        }
+    }
+}
